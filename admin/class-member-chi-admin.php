eb218aabc45ffa2cf9a3fa6c01af49198883cb71
Travis Northcutt <travis@brightagency.net>
2016-08-03 11:21:16 -0700

    Clean up admin class file

--- admin/class-member-chi-admin.php
+++ admin/class-member-chi-admin.php
@@ -3,19 +3,6 @@
 /**
  * The admin-specific functionality of the plugin.
  *
- * @link       https://memberup.co
- * @since      1.0.0
- *
- * @package    Member_Chi
- * @subpackage Member_Chi/admin
- */
-
-/**
- * The admin-specific functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the admin-specific stylesheet and JavaScript.
- *
  * @package    Member_Chi
  * @subpackage Member_Chi/admin
  * @author     Member Up <travis@memberup.co>
