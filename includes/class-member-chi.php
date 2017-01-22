154e622bfc6072ce79660c3bf1a46379771d425d
rclations <contact@rclations.com>
2017-01-21 20:10:03 -0500

    implementing WP Users csv export

--- includes/class-member-chi.php
+++ includes/class-member-chi.php
@@ -146,6 +146,8 @@ class Member_Chi {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		$this->loader->add_action( 'admin_init', $plugin_admin, 'csv_export' );
+
 	}
 
 	/**
