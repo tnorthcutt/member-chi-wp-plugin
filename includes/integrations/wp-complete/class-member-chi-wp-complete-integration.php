36dfe9f89033bfd17e400e89b0a945a7a1947f7b
rclations <contact@rclations.com>
2017-03-22 21:45:04 -0400

    enhancing wpcomplete integration

--- includes/integrations/wp-complete/class-member-chi-wp-complete-integration.php
+++ includes/integrations/wp-complete/class-member-chi-wp-complete-integration.php
@@ -33,6 +33,7 @@ class Member_Chi_Wp_Complete_Integration extends Member_Chi_Membership_Plugin_In
 	 */
 	private function define_hooks() {
 		add_action( 'wpcomplete_mark_completed', array( $this, 'mark_completed' ), 10, 1 );
+		add_action( 'wpcomplete_mark_incomplete', array( $this, 'mark_incomplete' ), 10, 1 );
 	}
 
 
@@ -53,11 +54,37 @@ class Member_Chi_Wp_Complete_Integration extends Member_Chi_Membership_Plugin_In
 			'wp_id' => $args['user_id'],
 			'team_id' => $this->team_hash,
 			'event_type' => 'post.complete',
+			'post_id' => $args['post_id'],
+			'button_id' => $args['button_id'],
 		);
 
 		$response = $this->post( $this->url, $body );
 
 	}
 
+	/**
+	 * @param array $args An array from WP Complete, consisting of:
+	 *      user_id
+	 *      post_id
+	 *      button_id
+	 *      post_status
+	 *
+	 */
+	public function mark_incomplete( $args ) {
+
+		$user = get_userdata( $args['user_id'] );
+
+		$body = array(
+			'email' => $user->user_email,
+			'wp_id' => $args['user_id'],
+			'team_id' => $this->team_hash,
+			'event_type' => $args['post_status'],
+			'post_id' => $args['post_id'],
+			'button_id' => $args['button_id'],
+		);
+
+		$response = $this->post( $this->url, $body );
+
+	}
 
 }
